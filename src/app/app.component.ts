4815e7fbd31c214c860c688f2c7cde050ae2a4a7
Matt Lunalover <mlunalover@kabbage.com>
2016-07-13 09:29:11 -0400

    added anothe custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
 import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @Component({
   moduleId: module.id,
   selector: 'directives-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [HighlightDirective]
+  directives: [HighlightDirective, UnlessDirective]
 })
 export class AppComponent {
   private switch = true;
   private items = [1 ,2 ,3 ,4 ,5];
+  private value = 100;
   onSwitch()  {
     this.switch = !this.switch;
   }
